29101ccf92da9e40535da536715f9c7d66fd7d4b
mdsoll <64893745+mdsoll@users.noreply.github.com>
2022-10-14 19:53:00 +0800

    Update split_featselection.R
    cleaning up code

--- split_featselection.R
+++ split_featselection.R
@@ -7,7 +7,6 @@ library(leaps)
 library(MASS)
 
 # downloading parsed data and split into train-test sets
-
 df <- read_csv("./data-features.csv")
 View(df)
 sample <- sample(c(1, 0), length(unique(df$gene_id)), replace=TRUE, prob=c(0.7,0.3))
@@ -20,10 +19,12 @@ nrow(test)
 train <- train[-c(19)]
 test <- test[-c(19)]
 head(train)
+
+# export split data
 write.csv(train,"train.csv", row.names = FALSE)
 write.csv(test,"test.csv", row.names = FALSE)
 
-
+# load data
 train <- read_csv("train.csv")
 View(train)
 summary(train)
@@ -45,6 +46,8 @@ full.model <- lm(label ~ mean_time1 + mean_time2 + mean_time3 +
                    combined_sd1 + combined_sd2 + combined_sd3 +
                    mean_cur1 + mean_cur2 + mean_cur3 +
                    DRACH1 + DRACH2 + DRACH3 + gtf_rel_len, data = train)
+# indicates that DRACH1, DRACH3 should be removed
+
 # Stepwise regression model
 step.model <- stepAIC(full.model, direction = "both", 
                       trace = FALSE)
@@ -55,7 +58,8 @@ model2 <- lm(label ~ mean_time1 + mean_time2 + mean_time3 +
                    combined_sd1 + combined_sd2 +
                    mean_cur1 + mean_cur2 + mean_cur3 +
                    DRACH1 + DRACH2 + DRACH3 + gtf_rel_len, data = train)
-# Stepwise regression model
+
 step.model <- stepAIC(model2, direction = "both", 
                       trace = FALSE)
 summary(step.model)
+# indicates DRACH1, DRACH3, combined_sd3 should be removed
