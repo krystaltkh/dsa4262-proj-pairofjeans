ace2c46be2e74958a82613b3abd4d046e6d79d66
Krystal Tay <e0425873@u.nus.edu>
2022-10-15 12:54:05 +0800

    updated with factor & selected features

--- over-sampling.R
+++ over-sampling.R
@@ -17,28 +17,32 @@ df <- df %>%
 # SMOTE, K = 5
 smoted5 <- SMOTE(df[,-c(1:4)], df$label, K = 5)
 smoted5_df <- smoted5$data %>%
-  select(-class)
+  select(-c(class, combined_sd3, DRACH1, DRACH3))
+smoted5_df$label <- as.factor(smoted5_df$label)
 
 # SMOTE, K = 10
 smoted10 <- SMOTE(df[,-c(1:4)], df$label, K = 10)
 smoted10_df <- smoted10$data %>%
-  select(-class)
+  select(-c(class, combined_sd3, DRACH1, DRACH3))
+smoted10_df$label <- as.factor(smoted10_df$label)
 
 # ADASYN, K = 5
 adas5 <- ADAS(df[,-c(1:4)], df$label, K = 5)
 adas5_df <- adas5$data %>%
-  select(-class)
+  select(-c(class, combined_sd3, DRACH1, DRACH3))
+adas5_df$label <- as.factor(adas5_df$label)
 
-<<<<<<< HEAD
 # ADASYN, K = 10
 adas10 <- ADAS(df[,-c(1:4)], df$label, K = 10)
 adas10_df <- adas10$data %>%
-  select(-class)
+  select(-c(class, combined_sd3, DRACH1, DRACH3))
+adas10_df$label <- as.factor(adas10_df$label)
 
 # density-based SMOTE
 dbsmoted <- DBSMOTE(df[,-c(1:4)], df$label)
 dbsmoted_df <- dbsmoted$data %>%
-  select(-class)
+  select(-c(class, combined_sd3, DRACH1, DRACH3))
+dbsmoted_df$label <- as.factor(dbsmoted_df$label)
 
 
 
@@ -52,7 +56,3 @@ dbsmoted_df <- dbsmoted$data %>%
 # 
 # ovun <- ovun.sample(label~., data=df, method="both", seed=4262)
 # ovun_df <- ovun$data
-=======
-ovun <- ovun.sample(label~., data=df, method="both", seed=4262)
-ovun_df <- ovun$data
->>>>>>> b3b50be6c41e7f6871333a4e489aedbd01928f3c
